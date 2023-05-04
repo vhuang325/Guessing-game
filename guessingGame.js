1c9e8943758f7dcd2609c918e57fe31c02173b01
vhuang325 <vhuang325@gmail.com>
2023-05-04 18:00:58 -0400

    added most things

--- guessingGame.js
+++ guessingGame.js
@@ -2,29 +2,38 @@ const goButton = document.querySelector("#goButton");
 const inputBar = document.querySelector("#inputBar");
 const resetButton = document.querySelector("#resetButton");
 const hintButton = document.querySelector("#hintButton");
-const guess1 = document.querySelector(".guesses")
+const feedback = document.querySelector("#feedback");
+const wrongGuess1 = document.querySelector("#wrongGuess1");
+const wrongGuess2 = document.querySelector("#wrongGuess2");
+const wrongGuess3 = document.querySelector("#wrongGuess3");
+const wrongGuess4 = document.querySelector("#wrongGuess4");
+const wrongGuess5 = document.querySelector("#wrongGuess5");
 
 
 let randomNumber = Math.ceil(Math.random() * 100);
 console.log(randomNumber);
-// randomNumber.textContent
+let remainingGuess = 0;
 
 goButton.addEventListener("click", function(){
     const guess = parseInt(inputBar.value);
-    console.log(`${guess}`);
+    wrongGuess1.textContent = (`${guess}`)
     let difference = Math.abs(guess - randomNumber);
+    remainingGuess++;
     if (guess === randomNumber){
-        console.log("YOU WIN!!!!");
-    } else if (difference <= 10){
-        if(guess < randomNumber){
-        console.log("HOT! Go alittle higher!");
-        } else {
-        console.log("HOT! Go alittle lower!")
+        feedback.textContent = "You Win!";
+        }if (remainingGuess === 5){
+            console.log("You Lose!");
+        }else if (difference <= 10){
+            if(guess < randomNumber){
+            console.log("HOT! Go alittle higher!");
+            } else {
+            console.log("HOT! Go alittle lower!")
+            }
+        }else if (guess < randomNumber){
+            console.log("You guessed too low");
+        } else if (guess > randomNumber){
+            console.log("You guessed too high");
+
         }
-    }else if (guess < randomNumber){
-        console.log("You guessed too low");
-    } else if (guess > randomNumber){
-        console.log("You guessed too high");
-    }
 
 }) 
