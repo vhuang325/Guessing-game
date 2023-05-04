e9ada6cd4ca9a5a98856d62ba41e4595ef9a21b1
vhuang325 <vhuang325@gmail.com>
2023-05-04 16:55:46 -0400

    added too hot alittle higher too lower alittle lower

--- guessingGame.js
+++ guessingGame.js
@@ -5,16 +5,23 @@ const hintButton = document.querySelector("#hintButton");
 const guess1 = document.querySelector(".guesses")
 
 
-let randomNumber = Math.floor(Math.random() * 100);
+let randomNumber = Math.ceil(Math.random() * 100);
 console.log(randomNumber);
 // randomNumber.textContent
 
 goButton.addEventListener("click", function(){
     const guess = parseInt(inputBar.value);
     console.log(`${guess}`);
+    let difference = Math.abs(guess - randomNumber);
     if (guess === randomNumber){
         console.log("YOU WIN!!!!");
-    } else if (guess < randomNumber){
+    } else if (difference <= 10){
+        if(guess < randomNumber){
+        console.log("HOT! Go alittle higher!");
+        } else {
+        console.log("HOT! Go alittle lower!")
+        }
+    }else if (guess < randomNumber){
         console.log("You guessed too low");
     } else if (guess > randomNumber){
         console.log("You guessed too high");
